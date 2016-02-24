c78f99cb9dc9b1aeaad84550733865108334e1b0
Alistair Miles <Alistair Miles>
2016-02-24 17:50:32 +0000

    Changed some settings

--- SysReview/SysReview/urls.py
+++ SysReview/SysReview/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from mainapp import views
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'SysReview.views.home', name='home'),
+    url(r'^$', 'mainapp.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
 )
