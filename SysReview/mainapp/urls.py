91a686a30579ef4a622b15760446ca236e2c1625
GlasIan <9905142l@gmail.com>
2016-02-27 17:46:56 +0000

    added skeleton views, associated html templates and url mappings

--- SysReview/mainapp/urls.py
+++ SysReview/mainapp/urls.py
@@ -5,4 +5,14 @@ from mainapp import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
+                       url(r'^profile/$',views.profile,name='profile'),
+                       url(r'^reviews/$',views.reviews,name='reviews'),
+                       url(r'^create_review/$',views.create_review,name='create_review'),
+                       url(r'^queries/$',views.queries,name='queries'),
+                       url(r'^create_query/$',views.create_query,name='create_query'),
+                       url(r'^query_results/$',views.query_results,name='query_results'),
+                       url(r'^abstract_pool/$',views.abstract_pool,name='abstract_pool'),
+                       url(r'^document_pool/$',views.document_pool,name='document_pool'),
+                       url(r'^final_pool/$',views.final_pool,name='final_pool'),
                        )
+
--- SysReview/mainapp/views.py
+++ SysReview/mainapp/views.py
@@ -1,7 +1,60 @@
 from django.shortcuts import render
 
-# Create your views here.
-
+#main home page
 def index(request):
     context_dict = {}
     return render(request, 'mainapp/index.html', context_dict)
+
+#view profile and edit profile
+def profile(request):
+    context_dict = {}
+
+    return render(request,'mainapp/profile.html',context_dict)
+
+#view saved reviews
+def reviews(request):
+    context_dict = {}
+
+    return render(request,'mainapp/reviews.html',context_dict)
+
+#created new reviews
+def create_review(request):
+    context_dict = {}
+
+    return render(request,'mainapp/create_review.html',context_dict)
+
+#view saved queries
+def queries(request):
+    context_dict = {}
+
+    return render(request,'mainapp/queries.html',context_dict)
+
+#create new query
+def create_query(request):
+    context_dict = {}
+
+    return render(request,'mainapp/create_query.html',context_dict)
+
+#view query results and authorise queries and add to abstract pool
+def query_results(request):
+    context_dict = {}
+
+    return render(request,'mainapp/query_results.html',context_dict)
+
+#view abstract pool and authorise abstracts and add to document pool
+def abstract_pool(request):
+    context_dict = {}
+
+    return render(request,'mainapp/abstract_pool.html',context_dict)
+
+#view document pool and authorise documents and add to final pool
+def document_pool(request):
+    context_dict = {}
+
+    return render(request,'mainapp/document_pool.html',context_dict)
+
+#view final pool and edit final pool
+def final_pool(request):
+    context_dict = {}
+
+    return render(request,'mainapp/final_pool.html',context_dict)
