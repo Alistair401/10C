3fd1d3efec5ca1636ca1826f66a863269cb6128f
GlasIan <9905142l@gmail.com>
2016-02-27 20:27:03 +0000

    added requirements.txt, set up some models

--- SysReview/mainapp/models.py
+++ SysReview/mainapp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+
 
 # Create your models here.
+class Review(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Review, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Query(models.Model):
+    review = models.ForeignKey(Review)
+    name = models.CharField(max_length=128, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Document(models.Model):
+    review = models.ForeignKey(Review)
+
+    title = models.TextField(unique=True)
+    authors = models.TextField()
+    abstract = models.TextField()
+    documentURL = models.URLField()
+    documentFree = models.BooleanField()
+    pubmedID = models.CharField(max_length=128)
+    citation = models.CharField(max_length=128)
+
+    abstractPool = models.BooleanField(default=True)
+    documentPool = models.BooleanField(default=False)
+    finalPool = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.title
--- SysReview/mainapp/urls.py
+++ SysReview/mainapp/urls.py
@@ -1,5 +1,3 @@
-__author__ = 'uni'
-
 from django.conf.urls import patterns, url
 from mainapp import views
 
