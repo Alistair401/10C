4b801f27748216540a87c1f9c4f19400eb0d1f59
GlasIan <9905142l@gmail.com>
2016-02-27 22:22:45 +0000

    set up admin with views to tables

--- SysReview/mainapp/admin.py
+++ SysReview/mainapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from mainapp.models import Review, Query, Document
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    list_display = ('pk','name')
+
+class QueryAdmin(admin.ModelAdmin):
+    list_display = ('pk','name','review')
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('pk','title','review','abstractPool','documentPool','finalPool')
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Query, QueryAdmin)
+admin.site.register(Document,DocumentAdmin)
